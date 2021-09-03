2cd6fc78fddd908877535fde6133de9454afdc8b
Minh Duc <ducmk254@gmail.com>
2021-09-03 12:53:34 +0700

    fix sendEmail func

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,32 +1,32 @@
 const nodemailer = require("nodemailer");
 const ErrorResponse = require("./errorResponse");
-const sendEmail = async (options,next)=>{
-    
-    try {
-        const transporter = await nodemailer.createTransport({
-            host:"smtp.gmail.com",
-            port:587,
-            secure:false,
-            // service: process.env.EMAIL_SERVICE,
-            auth:{
-                user:process.env.EMAIL_USERNAME,
-                pass:process.env.EMAIL_PASSWORD
-            }
-        });
-    
-        const mailOptions = {
-            from:"vipnho1501@gmail.com",
-            to:options.to,
-            subject:options.subject,
-            html:options.text
-        }
-        console.log("...test...333");
-        await transporter.sendMail(mailOptions);
-        console.log("...test...444");
-    } catch (error) {
-        console.log(error);
-        return next(new ErrorResponse(error.message,500));
-    }
-}
+const sendEmail = async (options, next) => {
+  try {
+    const transporter = await nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      // service: process.env.EMAIL_SERVICE,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+    // console.log('transporter: ',transporter);
+    const mailOptions = {
+      from: "vipnho1501@gmail.com",
+      to: options.to,
+      subject: options.subject,
+      html: options.text,
+    };
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log(error);
+    return next(new ErrorResponse(error.message, 500));
+  }
+};
 
 module.exports = sendEmail;
